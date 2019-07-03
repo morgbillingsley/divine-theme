456f8a78c1616535e0b0aabcf60b594a72ae1a5f
morgbillingsley <billingsley98.mb@gmail.com>
2019-07-03 15:06:12 -0700

    figure iv commit

--- functions.php
+++ functions.php
@@ -1,3 +1,44 @@
 <?php
+    // Theme Support
     add_theme_support( 'post-thumbnails' );
+
+    // Hooks
+    add_action('admin_post_nopriv_contact', 'contact_proc');
+    add_action('admin_post_contact', 'contact_proc');
+
+    // Functions
+    function contact_proc() {
+        // Post data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        $datetime = date('Y-m-d h:i:sa');
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $browser = get_browser(null, true);
+        $client = strval($browser['parent']);
+        $os = strval($browser['platform']);
+
+        $table = 'contacts';
+        $data = array(
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            'message' => $message,
+            'datetime' => $datetime,
+            'ip_address' => $ip,
+            'browser' => $client,
+            'operating_system' => $os
+        );
+        $format = array('%s','%s','%s','%s','%s','%s','%s','%s','%s');
+
+        global $wpdb;
+        $wpdb->insert($table, $data, $format);
+
+        // Send notification
+        $message = "A new contact form was submitted by {$fname} {$lname}.";
+
+        // Redirect to homepage
+        wp_redirect(get_site_url());
+    }
 ?>
