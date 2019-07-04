e976f53108854cfb260399e8db9326039862dde0
morgbillingsley <billingsley98.mb@gmail.com>
2019-07-03 19:00:18 -0700

    contact iv commit

--- functions.php
+++ functions.php
@@ -9,12 +9,12 @@
     // Functions
     function contact_proc() {
         // Post data
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $message = $_POST['message'];
+        $name = strval($_POST['name']);
+        $email = strval($_POST['email']);
+        $phone = strval($_POST['phone']);
+        $message = strval($_POST['message']);
         $datetime = date('Y-m-d h:i:sa');
-        $ip = $_SERVER['REMOTE_ADDR'];
+        $ip = strval($_SERVER['REMOTE_ADDR']);
         $browser = get_browser(null, true);
         $client = strval($browser['parent']);
         $os = strval($browser['platform']);
@@ -34,9 +34,6 @@
 
         global $wpdb;
         $wpdb->insert($table, $data, $format);
-
-        // Send notification
-        $message = "A new contact form was submitted by {$fname} {$lname}.";
 
         // Redirect to homepage
         wp_redirect('http://divineembodimenthypnotherapy.com/');
